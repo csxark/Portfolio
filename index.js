5423ddf4ddf20b553c8ae90a437053151b4ba005
csxark <arktandoncs@gmail.com>
2024-08-19 18:35:00 +0530

    commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// Navbar 
+function toggleMenu() {
+    const sideMenu = document.getElementById('sideMenu');
+    const hamburger = document.querySelector('.hamburger');
+    sideMenu.classList.toggle('open');
+    hamburger.classList.toggle('open');
+  }
+  
