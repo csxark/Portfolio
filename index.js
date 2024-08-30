c5bd7afc0c59fc9d0e74345fa8d00e3758ee61cc
csxark <arktandoncs@gmail.com>
2024-08-30 19:53:00 +0530

    main

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
 // Navbar 
-function toggleMenu() {
-    const sideMenu = document.getElementById('sideMenu');
-    const hamburger = document.querySelector('.hamburger');
-    sideMenu.classList.toggle('open');
-    hamburger.classList.toggle('open');
-  }
-  
